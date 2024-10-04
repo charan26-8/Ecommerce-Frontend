99fe72577779b98a82d31735472ec91f5e34811e
charan26-8 <charanmalli222@gmail.com>
2024-10-04 10:28:04 +0530

    Changes Swagger to Amazon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -101,6 +101,7 @@ const Navbar = ({ onSelectCategory, onSearch }) => {
     "Electronics",
     "Toys",
     "Fashion",
+    "Mouse",
   ];
   return (
     <>
@@ -108,7 +109,7 @@ const Navbar = ({ onSelectCategory, onSearch }) => {
         <nav className="navbar navbar-expand-lg fixed-top">
           <div className="container-fluid">
             <a className="navbar-brand" href="http://localhost:8080/swagger-ui/index.html">
-             Swagger UI
+           Amazon
             </a>
             <button
               className="navbar-toggler"
